f5fa6a23c735094084cf37c55d259c2aeace01cb
Yohei Shiina <yohei_shiina@YSMBA.local>
2019-06-20 10:24:37 +0900

    テスト：ユーザーAが作成したタスクが表示されない

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -19,5 +19,18 @@ describe 'タスク管理機能', type: :system do
         expect(page).to have_content '最初のタスク'
       end
     end
+    context 'ユーザーBがログインしている時' do
+      before do 
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'test2@example.com')
+        visit login_path
+        fill_in 'メールアドレス', with: 'test2@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button
+      end
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        expect(page).to have_no_content '最初のタスク'
+      end
+    end
   end
 end
